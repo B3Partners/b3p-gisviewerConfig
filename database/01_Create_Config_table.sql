8e4a60c4dcf87fa018809513a93be8b374a7673a
Boy de Wit <boydewit@b3partners.nl>
2010-06-16 13:18:49 +0000

    - Default inserts nu in create script geplaatst.

--- database/01_Create_Config_table.sql
+++ database/01_Create_Config_table.sql
@@ -6,3 +6,20 @@ CREATE TABLE configuratie
   "setting" character varying(255),
   "type" character varying(255)
 );
+
+insert into configuratie (property, propval, setting, "type") values ('useCookies', 'true', 'viewer', 'java.lang.Boolean');
+insert into configuratie (property, propval, setting, "type") values ('usePopup', 'false', 'viewer', 'java.lang.Boolean');
+insert into configuratie (property, propval, setting, "type") values ('useDivPopup', 'false', 'viewer', 'java.lang.Boolean');
+insert into configuratie (property, propval, setting, "type") values ('usePanelControls', 'true', 'viewer', 'java.lang.Boolean');
+insert into configuratie (property, propval, setting, "type") values ('multipleActiveThemas', 'false', 'viewer', 'java.lang.Boolean');
+insert into configuratie (property, propval, setting, "type") values ('dataframepopupHandle', 'false', 'viewer', 'java.lang.Boolean');
+insert into configuratie (property, propval, setting, "type") values ('showLeftPanel', 'null', 'viewer', 'java.lang.Boolean');
+insert into configuratie (property, propval, setting, "type") values ('tolerance', '1', 'viewer', 'java.lang.Integer');
+insert into configuratie (property, propval, setting, "type") values ('autoRedirect', '2', 'viewer', 'java.lang.Integer');
+insert into configuratie (property, propval, setting, "type") values ('useSortableFunction', 'false', 'viewer', 'java.lang.Boolean');
+insert into configuratie (property, propval, setting, "type") values ('layerDelay', '5000', 'viewer', 'java.lang.Integer');
+insert into configuratie (property, propval, setting, "type") values ('refreshDelay', '1000', 'viewer', 'java.lang.Integer');
+insert into configuratie (property, propval, setting, "type") values ('minBboxZoeken', '1000', 'viewer', 'java.lang.Integer');
+insert into configuratie (property, propval, setting, "type") values ('maxResults', '25', 'viewer', 'java.lang.Integer');
+insert into configuratie (property, propval, setting, "type") values ('expandAll', 'true', 'viewer', 'java.lang.Boolean');
+insert into configuratie (property, propval, setting, "type") values ('zoekConfigIds', '"1,2,7"', 'viewer', 'java.lang.String');
--- database/02_Insert_Config.sql
+++
@@ -1,4 +0,0 @@
-insert into configuratie (property, propval, setting, "type") VALUES ('useCookies','true','viewer','java.lang.Boolean');
-insert into configuratie (property, propval, setting, "type") VALUES ('usePopup','true','viewer','java.lang.Boolean');
-insert into configuratie (property, propval, setting, "type") VALUES ('useDivPopup','true','viewer','java.lang.Boolean');
-insert into configuratie (property, propval, setting, "type") VALUES ('usePanelControls','false','viewer','java.lang.Boolean');
