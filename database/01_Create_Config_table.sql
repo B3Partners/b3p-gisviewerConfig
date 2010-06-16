dc5b9581aa125b27e3b9b92691c44ff0e8a4ce4e
Boy de Wit <boydewit@b3partners.nl>
2010-06-16 13:57:43 +0000

    - zichtbare tabbladen in ConfigKeeper

--- database/01_Create_Config_table.sql
+++ database/01_Create_Config_table.sql
@@ -8,18 +8,22 @@ CREATE TABLE configuratie
 );
 
 insert into configuratie (property, propval, setting, "type") values ('useCookies', 'true', 'viewer', 'java.lang.Boolean');
-insert into configuratie (property, propval, setting, "type") values ('usePopup', 'false', 'viewer', 'java.lang.Boolean');
-insert into configuratie (property, propval, setting, "type") values ('useDivPopup', 'false', 'viewer', 'java.lang.Boolean');
-insert into configuratie (property, propval, setting, "type") values ('usePanelControls', 'true', 'viewer', 'java.lang.Boolean');
 insert into configuratie (property, propval, setting, "type") values ('multipleActiveThemas', 'false', 'viewer', 'java.lang.Boolean');
 insert into configuratie (property, propval, setting, "type") values ('dataframepopupHandle', 'false', 'viewer', 'java.lang.Boolean');
 insert into configuratie (property, propval, setting, "type") values ('showLeftPanel', 'null', 'viewer', 'java.lang.Boolean');
-insert into configuratie (property, propval, setting, "type") values ('tolerance', '1', 'viewer', 'java.lang.Integer');
 insert into configuratie (property, propval, setting, "type") values ('autoRedirect', '2', 'viewer', 'java.lang.Integer');
 insert into configuratie (property, propval, setting, "type") values ('useSortableFunction', 'false', 'viewer', 'java.lang.Boolean');
 insert into configuratie (property, propval, setting, "type") values ('layerDelay', '5000', 'viewer', 'java.lang.Integer');
 insert into configuratie (property, propval, setting, "type") values ('refreshDelay', '1000', 'viewer', 'java.lang.Integer');
 insert into configuratie (property, propval, setting, "type") values ('minBboxZoeken', '1000', 'viewer', 'java.lang.Integer');
+insert into configuratie (property, propval, setting, "type") values ('zoekConfigIds', '"1"', 'viewer', 'java.lang.String');
 insert into configuratie (property, propval, setting, "type") values ('maxResults', '25', 'viewer', 'java.lang.Integer');
+insert into configuratie (property, propval, setting, "type") values ('usePopup', 'false', 'viewer', 'java.lang.Boolean');
+insert into configuratie (property, propval, setting, "type") values ('useDivPopup', 'false', 'viewer', 'java.lang.Boolean');
+insert into configuratie (property, propval, setting, "type") values ('usePanelControls', 'true', 'viewer', 'java.lang.Boolean');
 insert into configuratie (property, propval, setting, "type") values ('expandAll', 'true', 'viewer', 'java.lang.Boolean');
-insert into configuratie (property, propval, setting, "type") values ('zoekConfigIds', '"1,2,7"', 'viewer', 'java.lang.String');
+insert into configuratie (property, propval, setting, "type") values ('tabbladenGebruiker', '"themas", "legenda", "zoeken"', 'viewer', 'java.lang.String');
+insert into configuratie (property, propval, setting, "type") values ('tabbladenBeheerder', '"themas", "legenda", "zoeken","analyse","informatie"', 'viewer', 'java.lang.String');
+insert into configuratie (property, propval, setting, "type") values ('tabbladenDemoGebruiker', '"themas", "legenda"', 'viewer', 'java.lang.String');
+insert into configuratie (property, propval, setting, "type") values ('tabbladenAnoniem', '"themas"', 'viewer', 'java.lang.String');
+insert into configuratie (property, propval, setting, "type") values ('tolerance', '1', 'viewer', 'java.lang.Integer');
