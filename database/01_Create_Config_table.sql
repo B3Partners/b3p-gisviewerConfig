dfe51570868b2b81d637c19c53a8bc80bf179ee8
Boy de Wit <boydewit@b3partners.nl>
2010-06-16 09:03:39 +0000

    Create configuratie tabel. Voor ConfigKeeper instellingen. Hier komen straks o.a. instellingen in die nu nog via viewer.jsp gedaan worden.

---
+++ database/01_Create_Config_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE configuratie
+(
+  "ID" serial NOT NULL,
+  "PROPERTY" character varying(255),
+  "PROPVAL" character varying,
+  "SETTING" character varying(255),
+  "TYPE" character varying(255)
+);
