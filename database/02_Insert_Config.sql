32b64e1e921d524d7b54c1bfefae0f9f97b8b538
Boy de Wit <boydewit@b3partners.nl>
2010-06-16 11:48:37 +0000

    Insert test config items

---
+++ database/02_Insert_Config.sql
@@ -0,0 +1,4 @@
+insert into configuratie (property, propval, setting, "type") VALUES ('useCookies','true','viewer','java.lang.Boolean');
+insert into configuratie (property, propval, setting, "type") VALUES ('usePopup','true','viewer','java.lang.Boolean');
+insert into configuratie (property, propval, setting, "type") VALUES ('useDivPopup','true','viewer','java.lang.Boolean');
+insert into configuratie (property, propval, setting, "type") VALUES ('usePanelControls','false','viewer','java.lang.Boolean');
