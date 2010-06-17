e4707666255b6823228c70be89ac51ffbda14b45
Boy de Wit <boydewit@b3partners.nl>
2010-06-17 15:24:19 +0000

    aantal settings eruit gehaald.

--- src/nl/b3p/gis/viewer/ConfigKeeperAction.java
+++ src/nl/b3p/gis/viewer/ConfigKeeperAction.java
@@ -1,6 +1,5 @@
 package nl.b3p.gis.viewer;
 
-import java.util.Collection;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,7 +8,6 @@ import nl.b3p.gis.viewer.db.Configuratie;
 import nl.b3p.gis.viewer.services.HibernateUtil;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.validator.DynaValidatorForm;
@@ -29,16 +27,11 @@ public class ConfigKeeperAction extends ViewerCrudAction {
         /* de getConfigMap geeft alle waardes terug volgens de types
          * zoals deze gedefinieerd zijn in het type kolom van de configuratie */
         boolean useCookies = (Boolean) map.get("useCookies");
-        boolean multipleActiveThemas = (Boolean) map.get("multipleActiveThemas");
-        boolean usePopup = (Boolean) map.get("usePopup");
         boolean useDivPopup = (Boolean) map.get("useDivPopup");
         boolean dataframepopupHandle = (Boolean) map.get("dataframepopupHandle");
         boolean usePanelControls = (Boolean) map.get("usePanelControls");
-        boolean showLeftPanel = (Boolean) map.get("showLeftPanel");
         int autoRedirect = (Integer) map.get("autoRedirect");
         int tolerance = (Integer) map.get("tolerance");
-        boolean useSortableFunction = (Boolean) map.get("useSortableFunction");
-        int layerDelay = (Integer) map.get("layerDelay");
         int refreshDelay = (Integer) map.get("refreshDelay");
         String zoekConfigIds = (String) map.get("zoekConfigIds");
         int minBboxZoeken = (Integer) map.get("minBboxZoeken");
@@ -51,16 +44,8 @@ public class ConfigKeeperAction extends ViewerCrudAction {
 
         /* config klaarzetten voor formulier */
         dynaForm.set("cfg_useCookies", useCookies);
-        dynaForm.set("cfg_multipleActiveThemas", multipleActiveThemas);
-        dynaForm.set("cfg_usePopup", usePopup);
-        dynaForm.set("cfg_useDivPopup", useDivPopup);
-        dynaForm.set("cfg_dataframepopupHandle", dataframepopupHandle);
-        dynaForm.set("cfg_usePanelControls", usePanelControls);
-        dynaForm.set("cfg_showLeftPanel", showLeftPanel);
         dynaForm.set("cfg_autoRedirect", autoRedirect);
         dynaForm.set("cfg_tolerance", tolerance);
-        dynaForm.set("cfg_useSortableFunction", useSortableFunction);
-        dynaForm.set("cfg_layerDelay", layerDelay);
         dynaForm.set("cfg_refreshDelay", refreshDelay);
         dynaForm.set("cfg_zoekConfigIds", zoekConfigIds);
         dynaForm.set("cfg_minBboxZoeken", minBboxZoeken);
@@ -70,6 +55,8 @@ public class ConfigKeeperAction extends ViewerCrudAction {
         dynaForm.set("cfg_tabbladenGebruiker", tabbladenGebruiker);
         dynaForm.set("cfg_tabbladenDemoGebruiker", tabbladenDemoGebruiker);
         dynaForm.set("cfg_tabbladenAnoniem", tabbladenAnoniem);
+
+        /* dropdown voor cfg_objectInfo ophalen en goedzetten */
 
         return super.unspecified(mapping, dynaForm, request, response);
     }
@@ -90,35 +77,11 @@ public class ConfigKeeperAction extends ViewerCrudAction {
         c = configKeeper.getConfiguratie("useCookies","viewer");
         writeBoolean(dynaForm, "cfg_useCookies", c);
 
-        c = configKeeper.getConfiguratie("multipleActiveThemas","viewer");
-        writeBoolean(dynaForm, "cfg_multipleActiveThemas", c);
-
-        c = configKeeper.getConfiguratie("usePopup","viewer");
-        writeBoolean(dynaForm, "cfg_usePopup", c);
-
-        c = configKeeper.getConfiguratie("useDivPopup","viewer");
-        writeBoolean(dynaForm, "cfg_useDivPopup", c);
-
-        c = configKeeper.getConfiguratie("dataframepopupHandle","viewer");
-        writeBoolean(dynaForm, "cfg_dataframepopupHandle", c);
-
-        c = configKeeper.getConfiguratie("usePanelControls","viewer");
-        writeBoolean(dynaForm, "cfg_usePanelControls", c);
-
-        c = configKeeper.getConfiguratie("usePanelControls","viewer");
-        writeBoolean(dynaForm, "cfg_showLeftPanel", c);
-
         c = configKeeper.getConfiguratie("autoRedirect","viewer");
         writeInteger(dynaForm, "cfg_autoRedirect", c);
 
         c = configKeeper.getConfiguratie("tolerance","viewer");
         writeInteger(dynaForm, "cfg_tolerance", c);
-
-        c = configKeeper.getConfiguratie("useSortableFunction","viewer");
-        writeBoolean(dynaForm, "cfg_useSortableFunction", c);
-
-        c = configKeeper.getConfiguratie("layerDelay","viewer");
-        writeInteger(dynaForm, "cfg_layerDelay", c);
 
         c = configKeeper.getConfiguratie("refreshDelay","viewer");
         writeInteger(dynaForm, "cfg_refreshDelay", c);
