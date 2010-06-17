2cfba8385c022d76d6add877ca4c4664e35bc686
Boy de Wit <boydewit@b3partners.nl>
2010-06-17 10:35:26 +0000

    showLeftPanel op false gezet ipv null

--- database/01_Create_Config_table.sql
+++ database/01_Create_Config_table.sql
@@ -10,7 +10,7 @@ CREATE TABLE configuratie
 insert into configuratie (property, propval, setting, "type") values ('useCookies', 'true', 'viewer', 'java.lang.Boolean');
 insert into configuratie (property, propval, setting, "type") values ('multipleActiveThemas', 'false', 'viewer', 'java.lang.Boolean');
 insert into configuratie (property, propval, setting, "type") values ('dataframepopupHandle', 'false', 'viewer', 'java.lang.Boolean');
-insert into configuratie (property, propval, setting, "type") values ('showLeftPanel', 'null', 'viewer', 'java.lang.Boolean');
+insert into configuratie (property, propval, setting, "type") values ('showLeftPanel', 'false', 'viewer', 'java.lang.Boolean');
 insert into configuratie (property, propval, setting, "type") values ('autoRedirect', '2', 'viewer', 'java.lang.Integer');
 insert into configuratie (property, propval, setting, "type") values ('useSortableFunction', 'false', 'viewer', 'java.lang.Boolean');
 insert into configuratie (property, propval, setting, "type") values ('layerDelay', '5000', 'viewer', 'java.lang.Integer');
